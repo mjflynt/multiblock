babb081420e5e56ade1bf239329a7e71876e6c1d
Jeffrey <mjflynt@gmail.com>
2022-12-23 01:18:30 -0700

    Added block parameters

--- multiblockcond.rb
+++ multiblockcond.rb
@@ -3,16 +3,16 @@ def f(x, y, &b)
     method(
         def jeff(s, t, p1, junk, &p2)
             if s <= t
-                p1.call
+                p1.call s, t
             else
-                p2.call
+                p2.call s, t
                 # yield
             end
         end
             ).curry[x, y, b]
 end
 
-p f(13,5){ "second is same or larger!" }[nil]{ "first is larger" }
+p f(13,5){ |a, b| "#{b} >= #{a}" }[nil]{ |a, b| "#{a} > #{b}" }
 p f(5,5){ "second is same or larger!" }[nil]{ "first is larger" }
 p f(5,13){ "second is same or larger!" }[nil]{ "first is larger" }
 p f('a','z'){ "second is same or larger!" }[nil]{ "first is larger" }
